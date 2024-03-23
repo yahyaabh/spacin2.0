5ba03dc045e6ecab30f3ff6b45e8b78051f9bdfe
yahya <yehia.abu.alhassan.01@gmail.com>
2024-03-24 00:17:08 +0200

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>
+        <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors:{
+      'purple':'#4c1c46',
+      "purple-light":"#a296ca"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
