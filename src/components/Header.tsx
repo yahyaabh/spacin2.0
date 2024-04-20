8d76050f14aa9fb4569c66bf6ac5dce19ddc0b4b
yahya <yehia.abu.alhassan.01@gmail.com>
2024-04-20 22:06:47 +0300

    welcome home-page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 function Header() {
   return (
-    <div className="w-full flex items-center justify-around mt-2">
+    <div className="w-full flex items-center justify-around  h-1/4">
         <h1 className="font-bold text-3xl">SPAC<span className="text-purple-light">IN</span></h1>
         <a href="https://yahyaabh.github.io/portfolio/"><button className="border-2 p-2 rounded-md border-black">about us</button></a>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import Header from "../components/Header"
+import space from "../assets/space.jpg"
+import { CiCircleChevDown } from "react-icons/ci";
 function Home() {
   return (
-    <div>
+    <div className="h-screen">
         <Header/>
+        <div className="flex mx-6 justify-center items-center h-3/4 lg-mx-0 lg:justify-evenly ">
+          <div className="md:items-between md:w-2/5 lg:text-xl lg:w-1/5 l">
+              <h1 className="text-xl font-bold mb-3 lg:text-3xl">Welcome to <span className="text-purple">spacin</span></h1>
+              <p className="">Use our website to discover amazing things about space, interact with nasa's latest technologies and more.</p>
+              <button className="text-purple mt-2 self-center">< CiCircleChevDown className=" w-10 h-10"/></button>
+          </div>
+          <img className="hidden w-2/5 h-2/5 rounded-md md:block md:w-2/5 md:h-2/5 lg:w-1/5 " src={space}></img>
+          
+        </div>
+        
     </div>
   )
 }
