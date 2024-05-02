af3ccbb655737669b275f2870b25bf68ed559bcf
yahya <yehia.abu.alhassan.01@gmail.com>
2024-05-02 23:40:30 +0300

    roverform form integration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header:React.FC = () => {
   return (
     <div className="w-full flex items-center justify-evenly  h-1/4">
         { location.pathname != '/' && 
-           ( <Link className=' border-2 p-2 rounded-md border-purple hover:bg-purple hover:text-white duration-100  ' to="../"><IoReturnUpBack /></Link>) 
+           ( <Link className=' border-2 p-2 hover:p-1 rounded-md border-purple hover:bg-purple hover:text-white duration-100  ' to="../"><IoReturnUpBack /></Link>) 
         }
         <h1 className="font-bold text-3xl  ">SPAC<span className="text-purple-light">IN</span></h1>
         {location.pathname == '/' && ( <a className="text-purple bg-white rounded-md   border-2 border-purple hover:bg-purple hover:text-white duration-100  " href="https://yahyaabh.github.io/portfolio/"><button className="p-2 ">about us</button></a>)}
--- src/components/RoverForm.tsx
+++ src/components/RoverForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {DevTool} from "@hookform/devtools"
 import {useForm} from "react-hook-form"
 
 //here we define the type for rovers again
@@ -9,21 +10,31 @@ type Rover = "curiosity"|"opportunity"|"spirit";
 interface Props  {
     setRover : React.Dispatch<React.SetStateAction<Rover>>;
 }
+type formData ={
+    rover: string
+}
 
 export const RoverForm:React.FC<Props> = ({setRover}) => {  
 
+    const {register,control,handleSubmit} = useForm<formData>();
 
-    const form = useForm();
+    const onSubmit = (data:formData) => {
+            console.log(data)
+    }
+
   return (
     <>
-        <form className='w-screen flex flex-col justify-center items-center'>
+        <form onSubmit={handleSubmit(onSubmit)} className='w-screen flex flex-col justify-center items-center'>
             <h1 className='p-2 font-bold text-md'>choose a rover:</h1>
-            <select className='w-2/5 bg-white text-black border border-black rounded-sm text-center md:w-1/4' >
-                <option value="curiosity">curiosity</option>
+            <select  {...register("rover")} className='w-2/5 bg-white text-black border border-black rounded-sm text-center md:w-1/4' >
+                <option  value="curiosity">curiosity</option>
                 <option value="opportunity">opportunity</option>
                 <option value="spirit">spirit</option>
             </select>
+            <button className='border-2 p-2 hover:p-1 m-2 rounded-md border-purple hover:bg-purple hover:text-white duration-200 '>submit</button>
+            
         </form>
+        <DevTool control={control}/>
     </>
   )
 }
