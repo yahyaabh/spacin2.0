1386e0655c239b9f382e8f145115999d4074b312
yahya <yehia.abu.alhassan.01@gmail.com>
2024-05-19 23:03:49 +0300

    get one image

--- src/components/RoverDisplay.tsx
+++ src/components/RoverDisplay.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useForm} from "react-hook-form";
 
 type Rover = "curiosity"|"opportunity"|"spirit";
@@ -16,12 +16,21 @@ type formData =  {
 export const RoverDisplay:React.FC<props> = ({cams,rover,date}) => {
 
     const { register,handleSubmit } = useForm<formData>();
+    const [pic,setPic] = useState<string>();
+
 
     const onSubmit = async(data:formData) => {
+        
         //make a request depending on date,cam,rover
-            console.log(data.cam);
-            console.log(date);
-            console.log(rover)
+            // console.log(data.cam);
+            // console.log(date);
+            // console.log(rover);
+
+            const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&camera=${data.cam}&api_key=${import.meta.env.VITE_SECRET_KEY}`, {method: "GET"});
+            const datas = await res.json();
+            setPic(datas.photos[0].img_src);
+            
+            console.log(pic);
     }
 
   return (
@@ -38,6 +47,7 @@ export const RoverDisplay:React.FC<props> = ({cams,rover,date}) => {
             </select>
             <button className='border-2 p-2 hover:p-1 hover:m-3 m-2 rounded-md border-purple bg-purple text-white hover:bg-white hover:text-purple duration-200 '>choose</button>
         </form>
+        //display image here
         }
     </>
   )
